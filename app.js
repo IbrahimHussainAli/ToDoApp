0b20e62b4b4212f2424a0db40bbe27275426d499
Ibrahim <ibs_7867@hotmail.com>
2020-10-04 22:26:18 +0100

    add filter for the todos

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const dateElement = document.getElementById('date');
+const filterOption = document.querySelector('.filter-todos');
 
 //Event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('change', filterTodo);
 
 //Functions 
 function addTodo(event){
@@ -62,6 +64,23 @@ function deleteCheck(e){
             completeButton.innerHTML = '<i class="far fa-check-circle"></i>';
         }
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = "none";
+                }
+        }
+    });
 }
 
 // get date
